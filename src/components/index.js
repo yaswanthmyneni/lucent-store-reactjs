e30864e217579512c13e0396b6bece478e85b407
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-06 22:25:48 +0530

    chore: import and export files

---
+++ src/components/index.js
@@ -0,0 +1,5 @@
+import { Navigation } from "./navigation/Navigation";
+import { Footer } from "./footer/Footer";
+import { Category } from "./category/Category";
+
+export { Navigation, Footer, Category };
