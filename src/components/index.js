7e3bd162679f64a66576b272aaa4c6234171a62f
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-10 10:11:23 +0530

    chore: import and export of IconWithBadge component

--- src/components/index.js
+++ src/components/index.js
@@ -1,5 +1,6 @@
 import { Navigation } from "./navigation/Navigation";
 import { Footer } from "./footer/Footer";
 import { Category } from "./category/Category";
+import { IconWithBadge } from "./icon-with-badge/IconWithBadge";
 
-export { Navigation, Footer, Category };
+export { Navigation, Footer, Category, IconWithBadge };
