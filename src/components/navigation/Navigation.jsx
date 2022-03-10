6ff0fc8bd125ddbb4bccf4de47f0245f2446c54b
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-10 10:10:19 +0530

    refactor: removed id attributes and added IconWithBadge comonent

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -1,33 +1,32 @@
 import "./navigation.css";
+import { IconWithBadge } from "../index";
 
 const Navigation = () => {
   return (
-    <header id="header" className="header">
+    <header className="header">
       <div className="navbar-container">
         <a className="navbar" href="./home.html">
           <h1 className="navbar-m-lr-1">LUCENT STORE</h1>
         </a>
-        <a className="navbar-m-left-auto navbar" href="../login/login.html">
+        <a className="navbar-m-left-auto navbar" href="#">
           <h5 className="navbar-m-lr-1">LOGIN</h5>
         </a>
         <div className="search-bar-border navbar-m-lr-1">
           <input className="input search-bar-input" type="text" />
           <i className="fas fa-search"></i>
         </div>
-        <a className="navbar navbar-m-lr-1" href="../product/product.html">
+        <a className="navbar navbar-m-lr-1" href="#">
           <i className="fa-solid fa-store"></i>
         </a>
-        <a className="navbar navbar-m-lr-1" href="../wishlist/wishlist.html">
-          <div className="display-badge pos-rel-badge">
+        <a className="navbar navbar-m-lr-1" href="#">
+          <IconWithBadge>
             <i className="fas fa-heart icon-badge"></i>
-            <span className="badge badge-online badge-to-icon">9</span>
-          </div>
+          </IconWithBadge>
         </a>
-        <a className="navbar navbar-m-lr-1 m-r-2rem" href="../cart/cart.html">
-          <div className="display-badge pos-rel-badge">
+        <a className="navbar navbar-m-lr-1 m-r-2rem" href="#">
+          <IconWithBadge>
             <i className="fas fa-shopping-cart icon-badge"></i>
-            <span className="badge badge-online badge-to-icon">9</span>
-          </div>
+          </IconWithBadge>
         </a>
       </div>
     </header>
