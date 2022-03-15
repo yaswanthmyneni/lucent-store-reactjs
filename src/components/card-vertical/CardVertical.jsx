75666e5af9b43f91143d153357141ec332bf50bb
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-15 13:09:24 +0530

    refactor: changed name from Card to CardVertical

--- src/components/card-vertical/CardVertical.jsx
+++ src/components/card-vertical/CardVertical.jsx
@@ -1,7 +1,13 @@
-import "./card.css";
+import "./card-vertical.css";
 import { Button } from "../index";
 
-const Card = ({ cardData, buttonPrimary, buttonSecondary }) => {
+const CardVertical = ({
+  cardData,
+  buttonPrimary,
+  buttonSecondary,
+  onClickFuncPrimary,
+  onClickFuncSecondary,
+}) => {
   const {
     badgeName,
     image,
@@ -11,6 +17,7 @@ const Card = ({ cardData, buttonPrimary, buttonSecondary }) => {
     discountPrice,
     originalPrice,
     discountPercentage,
+    rating,
   } = cardData;
 
   return (
@@ -18,25 +25,37 @@ const Card = ({ cardData, buttonPrimary, buttonSecondary }) => {
       <span className="badge-text card-badge-pos flex-badge">{badgeName}</span>
       <img className="image-resp" src={image} alt="cart" />
       <div className="card-margin">
-        <p className="card-margin-bottom text-gray">{category}</p>
+        <div className="card-flex">
+        <p className="card-margin-bottom text-gray ">{category}</p>
+        <p className="card-margin-bottom">{rating}
+        <i className="fa-solid fa-star"></i></p>
+        </div>
         <h3 className="card-margin-bottom">{cardName}</h3>
         <p className="card-margin-bottom">{brandName}</p>
         <div className="card-margin-bottom card-align-items">
           <h3 className="card-margin-0 card-display card-margin-lr">
-            {discountPrice}
+            ${discountPrice}
           </h3>
           <h4 className="card-margin-0 card-display card-text-strick text-gray card-margin-lr">
-            {originalPrice}
+            ${originalPrice}
           </h4>
           <h3 className="card-margin-0 card-display card-discount card-margin-lr">
             {discountPercentage}
           </h3>
         </div>
       </div>
-      <Button name={buttonPrimary} className="btn-primary"></Button>
-      <Button name={buttonSecondary} className="btn-secondary"></Button>
+      <Button
+        name={buttonPrimary}
+        className="btn-primary"
+        onClickFunc={onClickFuncPrimary}
+      ></Button>
+      <Button
+        name={buttonSecondary}
+        className="btn-secondary"
+        onClickFunc={onClickFuncSecondary}
+      ></Button>
     </div>
   );
 };
 
-export { Card };
+export { CardVertical };
