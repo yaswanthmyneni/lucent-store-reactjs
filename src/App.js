56ca1edf3c0429a1588c326c74d3dc8f70c95461
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-20 12:27:22 +0530

    feat: added routes to authentication pages

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
 import React from "react";
-import { LandingPage, WishlistPage, ProductPage, CartPage } from "./pages";
-import { Routes, Route } from "react-router-dom";
+import {
+  LandingPage,
+  ProductPage,
+  WishlistPage,
+  CartPage,
+  SignInPage,
+  SignUpPage,
+  LogoutPage,
+} from "./pages";
+import { Route, Routes } from "react-router-dom";
 import { Footer, Navigation } from "./components";
 
 const App = () => {
   return (
     <>
       <Navigation />
+
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/products" element={<ProductPage />} />
         <Route path="/wishlist" element={<WishlistPage />} />
         <Route path="/cart" element={<CartPage />} />
+        <Route path="/signin" element={<SignInPage />} />
+        <Route path="/signup" element={<SignUpPage />} />
+        <Route path="/logout" element={<LogoutPage />} />
       </Routes>
+
       <Footer />
     </>
   );
