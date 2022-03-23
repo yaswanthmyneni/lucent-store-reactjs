191b604c7ce2c3d051a1774db49f6eb422b84a98
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-23 09:32:42 +0530

    refactor: decreased the code lines

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -25,59 +25,45 @@ const Navigation = () => {
           <h1 className="navbar-m-lr-1">LUCENT STORE</h1>
         </NavLink>
         {!encodedToken &&
-          path.pathname !== "/logout" &&
-          path.pathname !== "/signin" &&
-          path.pathname !== "/signup" && (
+          (path.pathname === "/products" || path.pathname === "/") && (
             <NavLink className="navbar-m-left-auto navbar" to="/signin">
               <h5 className="navbar-m-lr-1">LOGIN</h5>
             </NavLink>
           )}
-        {encodedToken &&
-          path.pathname !== "/logout" &&
-          path.pathname !== "/signin" &&
-          path.pathname !== "/signup" && (
-            <NavLink
-              className="navbar-m-left-auto navbar"
-              to="/logout"
-              onClick={() => localStorage.removeItem("token")}
-            >
-              <h5 className="navbar-m-lr-1">LOGOUT</h5>
-            </NavLink>
-          )}
-        {path.pathname !== "/logout" &&
-          path.pathname !== "/signin" &&
-          path.pathname !== "/signup" && (
-            <div className="search-bar-border navbar-m-lr-1">
-              <input className="input search-bar-input" type="text" />
-              <i className="fas fa-search"></i>
-            </div>
-          )}
-        {path.pathname !== "/products" &&
-          path.pathname !== "/logout" &&
-          path.pathname !== "/signin" &&
-          path.pathname !== "/signup" && (
-            <NavLink className="navbar navbar-m-lr-1" to="/products">
-              <i className="fa-solid fa-store"></i>
-            </NavLink>
-          )}
-        {encodedToken &&
-          path.pathname !== "/signin" &&
-          path.pathname !== "/signup" && (
-            <NavLink to="/wishlist">
-              <IconWithBadge count={wishlist.length}>
-                <i className="fas fa-heart icon-badge"></i>
-              </IconWithBadge>
-            </NavLink>
-          )}
-        {encodedToken &&
-          path.pathname !== "/signin" &&
-          path.pathname !== "/signup" && (
-            <NavLink to="/cart">
-              <IconWithBadge className="m-r-2rem" count={cartList.length}>
-                <i className="fas fa-shopping-cart icon-badge"></i>
-              </IconWithBadge>
-            </NavLink>
-          )}
+        {encodedToken && path.pathname !== "/logout" && (
+          <NavLink
+            className="navbar-m-left-auto navbar"
+            to="/logout"
+            onClick={() => localStorage.removeItem("token")}
+          >
+            <h5 className="navbar-m-lr-1">LOGOUT</h5>
+          </NavLink>
+        )}
+        {path.pathname === "/products" && (
+          <div className="search-bar-border navbar-m-lr-1">
+            <input className="input search-bar-input" type="text" />
+            <i className="fas fa-search"></i>
+          </div>
+        )}
+        {path.pathname !== "/products" && path.pathname !== "/logout" && (
+          <NavLink className="navbar navbar-m-lr-1" to="/products">
+            <i className="fa-solid fa-store"></i>
+          </NavLink>
+        )}
+        {encodedToken && (
+          <NavLink to="/wishlist">
+            <IconWithBadge count={wishlist.length}>
+              <i className="fas fa-heart icon-badge"></i>
+            </IconWithBadge>
+          </NavLink>
+        )}
+        {encodedToken && (
+          <NavLink to="/cart">
+            <IconWithBadge className="m-r-2rem" count={cartList.length}>
+              <i className="fas fa-shopping-cart icon-badge"></i>
+            </IconWithBadge>
+          </NavLink>
+        )}
       </div>
     </header>
   );
