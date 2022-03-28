a2cc2894dc02d3756a959e02c4a4b3fa429f5f9c
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-28 15:03:35 +0530

    refactor: added images to products and category files

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -1,5 +1,5 @@
 import { v4 as uuid } from "uuid";
-
+import cardImage from "../../assets/images/hero.jpg";
 /**
  * Category Database can be added here.
  * You can add category of your wish with different attributes
@@ -8,19 +8,23 @@ import { v4 as uuid } from "uuid";
 export const categories = [
   {
     _id: uuid(),
-    categoryName: "fiction",
+    categoryName: "shirt",
+    image:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQQq0lD1n_lrZaei9kboSj2UrYaiVirMUbgMw&usqp=CAU",
     description:
       "literature in the form of prose, especially novels, that describes imaginary events and people",
   },
   {
     _id: uuid(),
-    categoryName: "non-fiction",
+    categoryName: "pant",
+    image: "https://m.media-amazon.com/images/I/61bKHkUCNcL._UX569_.jpg",
     description:
       "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
   },
   {
     _id: uuid(),
-    categoryName: "horror",
+    categoryName: "yogamat",
+    image: cardImage,
     description:
       "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
   },
