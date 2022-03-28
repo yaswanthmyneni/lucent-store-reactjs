097f76dc6c0447c27981b7fcb8d99dac465e5b14
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-03-28 15:02:55 +0530

    refactor: changed names of files and also changes style of CategoryCard

--- src/components/index.js
+++ src/components/index.js
@@ -1,6 +1,6 @@
 export { Navigation } from "./navigation/Navigation";
 export { Footer } from "./footer/Footer";
-export { Category } from "./category/Category";
+export { CategoryCard } from "./category-card/CategoryCard";
 export { IconWithBadge } from "./icon-with-badge/IconWithBadge";
 export { CardVertical } from "./card-vertical/CardVertical";
 export { AsideBar } from "./aside-bar/AsideBar";
