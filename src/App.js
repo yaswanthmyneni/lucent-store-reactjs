82eec861572e4754d57f4dfdf91dde1ac0ff1066
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-04-06 08:57:34 +0530

    feat: created separate component for routing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,11 @@
 import React from "react";
-import {
-  LandingPage,
-  ProductPage,
-  WishlistPage,
-  CartPage,
-  SignInPage,
-  SignUpPage,
-  LogoutPage,
-  ErrorPage,
-} from "./pages";
-import { Route, Routes } from "react-router-dom";
-import { Footer, Navigation } from "./components";
+import { Footer, Navigation, Routing } from "./components";
 
 const App = () => {
   return (
     <>
       <Navigation />
-
-      <Routes>
-        <Route path="*" element={<ErrorPage />} />
-        <Route path="/" element={<LandingPage />} />
-        <Route path="/products" element={<ProductPage />} />
-        <Route path="/wishlist" element={<WishlistPage />} />
-        <Route path="/cart" element={<CartPage />} />
-        <Route path="/signin" element={<SignInPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/logout" element={<LogoutPage />} />
-      </Routes>
-
+      <Routing />
       <Footer />
     </>
   );
