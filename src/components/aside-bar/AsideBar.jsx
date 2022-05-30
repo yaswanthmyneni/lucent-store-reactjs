5e3dae529bef2bf7fedb821d82c58d2303d2fe51
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-05-30 10:36:44 +0530

    refactor: added cursor pointer

--- src/components/aside-bar/AsideBar.jsx
+++ src/components/aside-bar/AsideBar.jsx
@@ -37,10 +37,11 @@ const AsideBar = () => {
           <ul className="ul-none aside-ul">
             <h5>Price Range</h5>
             <li>
-              <label htmlFor="range">
+              <label htmlFor="range" className="cursor">
                 <input
                   type="range"
                   id="range"
+                  className="cursor"
                   min="0"
                   max="500"
                   step="50"
@@ -59,50 +60,53 @@ const AsideBar = () => {
           <ul className="ul-none aside-ul">
             <h5>Category</h5>
             <li>
-              <label htmlFor="yogamats">
+              <label htmlFor="yogamats" className="cursor">
                 <input
                   type="checkbox"
                   checked={yogamats}
                   id="yogamats"
+                  className="cursor"
                   onChange={(event) =>
                     productPageDispatch({
                       type: "YOGA_MATS",
                       payload: event.target.checked,
                     })
                   }
-                />{" "}
+                />
                 Yoga Mats
               </label>
             </li>
             <li>
-              <label htmlFor="shirts">
+              <label htmlFor="shirts" className="cursor">
                 <input
                   type="checkbox"
                   checked={shirts}
                   id="shirts"
+                  className="cursor"
                   onChange={(event) =>
                     productPageDispatch({
                       type: "SHIRTS",
                       payload: event.target.checked,
                     })
                   }
-                />{" "}
+                />
                 Shirts
               </label>
             </li>
             <li>
-              <label htmlFor="pants">
+              <label htmlFor="pants" className="cursor">
                 <input
                   type="checkbox"
                   checked={pants}
                   id="pants"
+                  className="cursor"
                   onChange={(event) =>
                     productPageDispatch({
                       type: "PANTS",
                       payload: event.target.checked,
                     })
                   }
-                />{" "}
+                />
                 Pants
               </label>
             </li>
@@ -110,29 +114,31 @@ const AsideBar = () => {
           <ul className="ul-none aside-ul">
             <h5>Sort By</h5>
             <li>
-              <label htmlFor="low-to-high">
+              <label htmlFor="low-to-high" className="cursor">
                 <input
                   type="radio"
                   id="low-to-high"
                   name="price"
+                  className="cursor"
                   onChange={() =>
                     productPageDispatch({ type: "SORT_BY", payload: false })
                   }
-                />{" "}
+                />
                 Price - Low to high
               </label>
             </li>
             <li>
-              <label htmlFor="high-to-low">
+              <label htmlFor="high-to-low" className="cursor">
                 <input
                   type="radio"
                   id="high-to-low"
                   name="price"
                   checked={sortBy}
+                  className="cursor"
                   onChange={() =>
                     productPageDispatch({ type: "SORT_BY", payload: true })
                   }
-                />{" "}
+                />
                 Price - High to low
               </label>
             </li>
@@ -140,54 +146,58 @@ const AsideBar = () => {
           <ul className="ul-none aside-ul">
             <h5>Rating</h5>
             <li>
-              <label htmlFor="star-4">
+              <label htmlFor="star-4" className="cursor">
                 <input
                   type="radio"
                   id="star-4"
                   name="rating"
+                  className="cursor"
                   onChange={() =>
                     productPageDispatch({ type: "RATING", payload: 4 })
                   }
-                />{" "}
+                />
                 4 Star & above
               </label>
             </li>
             <li>
-              <label htmlFor="star-3">
+              <label htmlFor="star-3" className="cursor">
                 <input
                   type="radio"
                   id="star-3"
                   name="rating"
+                  className="cursor"
                   onChange={() =>
                     productPageDispatch({ type: "RATING", payload: 3 })
                   }
-                />{" "}
+                />
                 3 Star & above
               </label>
             </li>
             <li>
-              <label htmlFor="star-2">
+              <label htmlFor="star-2" className="cursor">
                 <input
                   type="radio"
                   id="star-2"
                   name="rating"
+                  className="cursor"
                   onChange={() =>
                     productPageDispatch({ type: "RATING", payload: 2 })
                   }
-                />{" "}
+                />
                 2 Star & above
               </label>
             </li>
             <li>
-              <label htmlFor="star-1">
+              <label htmlFor="star-1" className="cursor">
                 <input
                   type="radio"
                   id="star-1"
                   name="rating"
+                  className="cursor"
                   onChange={() =>
                     productPageDispatch({ type: "RATING", payload: 1 })
                   }
-                />{" "}
+                />
                 1 Star & above
               </label>
             </li>
