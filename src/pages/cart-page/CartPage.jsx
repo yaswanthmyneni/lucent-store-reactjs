51f91d09b0fbf322a908b9082994fb503dcf23fa
yaswanthmyneni <79623546+yaswanthmyneni@users.noreply.github.com>
2022-05-30 11:14:32 +0530

    feat: added empty message in cart, wishlist ,and orders pages

--- src/pages/cart-page/CartPage.jsx
+++ src/pages/cart-page/CartPage.jsx
@@ -21,6 +21,7 @@ const CartPage = () => {
   // from toast context
   const { toastDispatch } = useToastContext();
 
+  // from react-router-dom
   const navigate = useNavigate();
 
   // cart price section calculations
@@ -36,40 +37,51 @@ const CartPage = () => {
   const totalPrice = price - discountPrice;
 
   return (
-    <>
-      <main className="cart-main">
-        <h2 className="text-center">MY CART</h2>
-        <div className="cart-page-wrapper">
-          <section className="cart-product">
-            {cartList.length > 0 &&
-              cartList.map((item) => (
-                <CardHorizontal
-                  key={item._id}
-                  cardData={item}
-                  onClickFunc1={() => {
-                    addToWishlist(wishlist, setWishlist, item, toastDispatch);
-                    removeFromCart(cartDispatch, item, toastDispatch);
-                  }}
-                  onClickFunc2={() => {
-                    removeFromCart(cartDispatch, item, toastDispatch);
-                  }}
-                />
-              ))}
-          </section>
-
-          <section className="price">
-            {cartList.length > 0 && (
-              <CartPrice
-                price={price}
-                discountPrice={discountPrice}
-                totalPrice={totalPrice}
-                onClickHandler={() => navigate("/checkout")}
-              />
-            )}
-          </section>
+    <main className="cart-main">
+      {cartList.length === 0 ? (
+        <div className="text-center mt-6rem">
+          <h3>Cart is empty!</h3>
+          <button
+            className="btn btn-primary"
+            onClick={() => navigate("/products")}
+          >
+            Go to store
+          </button>
         </div>
-      </main>
-    </>
+      ) : (
+        <>
+          <h2 className="text-center">MY CART</h2>
+          <div className="cart-page-wrapper">
+            <section className="cart-product">
+              {cartList.length > 0 &&
+                cartList.map((item) => (
+                  <CardHorizontal
+                    key={item._id}
+                    cardData={item}
+                    onClickFunc1={() => {
+                      addToWishlist(wishlist, setWishlist, item, toastDispatch);
+                      removeFromCart(cartDispatch, item, toastDispatch);
+                    }}
+                    onClickFunc2={() => {
+                      removeFromCart(cartDispatch, item, toastDispatch);
+                    }}
+                  />
+                ))}
+            </section>
+            <section className="price">
+              {cartList.length > 0 && (
+                <CartPrice
+                  price={price}
+                  discountPrice={discountPrice}
+                  totalPrice={totalPrice}
+                  onClickHandler={() => navigate("/checkout")}
+                />
+              )}
+            </section>
+          </div>
+        </>
+      )}
+    </main>
   );
 };
 
